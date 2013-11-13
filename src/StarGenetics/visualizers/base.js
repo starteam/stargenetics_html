c11e191defb8316fc8e4415a43c057db5a051267
Ivica Ceraj <iceraj@gmail.com>
2013-11-13 16:26:57 -0500

    New code in base

--- src/StarGenetics/visualizers/base.js
+++ src/StarGenetics/visualizers/base.js
@@ -4,7 +4,22 @@ define(["require", "exports"], function(require, exports) {
             this.width = 75;
             this.height = 75;
         }
-        BaseVisualizer.prototype.render = function (canvas, properties) {
+        BaseVisualizer.prototype.prepare = function (canvas) {
+            canvas.setAttribute('width', '' + this.width);
+            canvas.setAttribute('height', '' + this.height);
+            var context = canvas.getContext("2d");
+            context.save();
+            context.scale(this.width / 4, this.height / 4);
+            context.translate(1, 1);
+            return context;
+        };
+
+        BaseVisualizer.prototype.clearImage = function (context) {
+            context.clearRect(-1, -1, 1, 1);
+        };
+
+        BaseVisualizer.prototype.commit = function (context) {
+            context.restore();
         };
         return BaseVisualizer;
     })();
